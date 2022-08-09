de354a58e391bee1e5c2ddf8f4bd568ab84fb91c
Carlos Londono <c.londono.m@gmail.com>
2022-08-08 20:00:31 -0500

    configuring form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,42 @@
+import { useState } from 'react';
+
 const Form = () => {
-  return <div>Form</div>;
+  const [search, setSearch] = useState({
+    artist: '',
+    song: '',
+  });
+  return (
+    <form>
+      <legend>Search by artist and song</legend>
+      <div className='form-grid'>
+        <div>
+          <label htmlFor='artist'>Artist</label>
+          <input
+            type='text'
+            name='artist'
+            placeholder='Artist'
+            value={search.artist}
+            onChange={(e) =>
+              setSearch({ ...search, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor='song'>Song</label>
+          <input
+            type='text'
+            name='song'
+            placeholder='Song'
+            value={search.song}
+            onChange={(e) =>
+              setSearch({ ...search, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+        <input type='submit' value='Search' />
+      </div>
+    </form>
+  );
 };
 
 export default Form;
