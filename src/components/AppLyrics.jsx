7928753308c2a84be1d4c76897c9437facf37ade
Carlos Londono <c.londono.m@gmail.com>
2022-08-08 19:14:38 -0500

    creating applyrics and form comps

---
+++ src/components/AppLyrics.jsx
@@ -0,0 +1,13 @@
+import Form from './Form';
+
+const AppLyrics = () => {
+  return (
+    <>
+      <header>Lyrics Finder</header>
+      <Form />
+      <main></main>
+    </>
+  );
+};
+
+export default AppLyrics;
